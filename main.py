6475a9366e456cee5051f02e5d6ad2f8ab6d9e75
agyeirichmondowusu <85127347+agyeirichmondowusu@users.noreply.github.com>
2024-08-30 13:25:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -52,13 +52,13 @@ async def stream_updates():
         # Simulate an update every 5 seconds                                                                                                
         await asyncio.sleep(3)
 
-        json_data = json.dumps(data)
+        # json_data = json.dumps(data)
         
         # Yield the JSON data
-        yield f"data: {json_data}\n\n"
+        # yield f"data: {json_data}\n\n"
         
         # You can modify the data here dynamically if needed
-        #yield f"data: {data}\n\n"
+        yield f"data: {data}\n\n"
 
 @app.get("/stream")
 async def stream():
